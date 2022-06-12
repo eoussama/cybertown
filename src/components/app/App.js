922a671975e91a8aba31e010aa345d94dd235b96
EOussama <oussama_gtacj@live.fr>
2022-06-12 16:15:27 +0100

    😳 Pluct

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -15,31 +15,31 @@ function App() {
         </ParallaxLayer>
 
         <main className={classes['app__body']}>
-          <ParallaxLayer style={{maxHeight: '925px'}}>
+          <ParallaxLayer speed={-0.2}>
             <Home />
           </ParallaxLayer>
 
-          <ParallaxLayer offset={0.68} speed={0.05} className={classes.test}>
+          <ParallaxLayer offset={0.68} speed={0.05}>
             <div className={`${classes['skyline']} ${classes['skyline--5']}`}></div>
           </ParallaxLayer>
 
-          <ParallaxLayer offset={0.7} speed={0.1} className={classes.test}>
+          <ParallaxLayer offset={0.7} speed={0.1}>
             <div className={`${classes['skyline']} ${classes['skyline--4']}`}></div>
           </ParallaxLayer>
 
-          <ParallaxLayer offset={0.755} speed={0.2} className={classes.test}>
+          <ParallaxLayer offset={0.755} speed={0.2}>
             <div className={`${classes['skyline']} ${classes['skyline--3']}`}></div>
           </ParallaxLayer>
 
-          <ParallaxLayer offset={0.838} speed={0.36} className={classes.test}>
+          <ParallaxLayer offset={0.838} speed={0.36}>
             <div className={`${classes['skyline']} ${classes['skyline--2']}`}></div>
           </ParallaxLayer>
 
-          <ParallaxLayer offset={0.98} speed={0.6} className={classes.test}>
+          <ParallaxLayer offset={0.98} speed={0.6}>
             <div className={`${classes['skyline']} ${classes['skyline--1']}`}></div>
           </ParallaxLayer>
 
-          <ParallaxLayer offset={1.2}>
+          <ParallaxLayer offset={1} speed={1}>
             <About />
           </ParallaxLayer>
         </main>
