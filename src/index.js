f1487177b8c4d000a26989602083c90524383eec
EOussama <oussama_gtacj@live.fr>
2022-06-12 21:31:39 +0100

    🎨 Theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-
 import App from './components/app/App';
-
 import './index.scss';
-import './augmented-ui.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
