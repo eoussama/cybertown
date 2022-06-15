148d0da2321422b39e5ecc4447d81f69702c2bb0
EOussama <oussama_gtacj@live.fr>
2022-06-15 23:42:28 +0100

    ⌛ Optimize button hash, resolves #9

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,26 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import getHash from '../../utils/helpers/hash.helper';
 import classes from './Button.module.scss';
 
 export function Button(props) {
+  const hashTime = 100;
+
   const label = props.label || 'Button';
   const small = props.small || false;
 
   const smallClass = `${small ? classes['button-wrapper--small'] : ''}`;
 
+  const [isHovered, setIsHovered] = useState(false);
   const [hash, setHash] = useState(getHash());
+  const interval = useRef();
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setHash(getHash());
-    }, 100);
-    return () => clearInterval(interval);
-  }, []);
+    if (isHovered) {
+      interval.current = setInterval(() => {
+        setHash(getHash());
+      }, hashTime);
+    } else {
+      clearInterval(interval.current);
+    }
+
+    return () => clearInterval(interval.current);
+  }, [isHovered]);
 
   return (
     <button
       type="button"
       className={`${classes['button-wrapper']} ${smallClass}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <div
         data-augmented-ui="border bl-clip"
