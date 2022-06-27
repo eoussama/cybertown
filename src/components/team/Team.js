6f300037b88761e7382c06d74e0c9bd854a6efbb
EOussama <oussama_gtacj@live.fr>
2022-06-27 22:35:58 +0100

    🧑‍⚖️ Team section, resolves #12

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,7 +1,37 @@
 import { Title } from '../title/Title';
 import classes from './Team.module.scss';
+import { team } from './../../utils/consts/team.const';
+import Tilt from 'react-parallax-tilt';
 
 export function Team() {
+
+  const members = team.map((member, i) => (
+    <Tilt
+      className={classes['parallax']}
+      perspective={500}
+      glareEnable={false}
+      scale={1.3}
+    >
+      <div
+        key={i}
+        className={classes['member']}
+      >
+        <div
+          className={classes['member__wrapper']}
+          data-augmented-ui="border bl-clip tl-2-clip-x"
+        >
+          <div className={classes['member__picture']}>
+            <img src={member.picture} alt={member.name + "'s picture"} className='picture'></img>
+          </div>
+        </div>
+
+        <div className={classes['member__name']}>
+          {member.name}
+        </div>
+      </div>
+    </Tilt>
+  ));
+
   return (
     <div className={classes.team}>
       <div className={classes['team__wrapper']}>
@@ -10,11 +40,15 @@ export function Team() {
         </div>
 
         <div className={classes['team__body']}>
-          <p>
-            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+          <p className={classes['team__description']}>
+            Ri sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
           </p>
+
+          <div className={classes['team__members']}>
+            {members}
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
---
+++ src/utils/consts/team.const.js
@@ -0,0 +1,14 @@
+export const team = [
+  {
+    name: 'John Doe',
+    picture: 'https://i.pinimg.com/564x/59/7c/43/597c43c86327a7e9c642b2ae00d6a2ee.jpg'
+  },
+  {
+    name: 'Alex Beyc',
+    picture: 'https://i.pinimg.com/564x/9c/4c/e7/9c4ce74a69bdd3bf6d968c8893e7ca38.jpg'
+  },
+  {
+    name: 'Trix Mach',
+    picture: 'https://i.pinimg.com/564x/8f/39/eb/8f39eb89be4b84f9c9e3a73766837753.jpg'
+  }
+];
